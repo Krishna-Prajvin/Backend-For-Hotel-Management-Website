d37c8572b3210daf34aca9fda0f6cf5888b6e0b9
Krishna <118070745+Krishna-Prajvin@users.noreply.github.com>
2024-04-03 13:31:42 +0530

    Delete Login/review directory

--- Login/review/src/main/java/com/hotelmsbackend/review/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hotelmsbackend.review.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.hotelmsbackend.review.model.Customer;
-public interface CustomerRepository  extends JpaRepository<Customer,String> {
-
-    
-}
